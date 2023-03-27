da43973a14fad9a33f3ec5cc0e12d9e96f742586
winteeeee <b12e34e56@naver.com>
2023-03-27 10:56:31 +0900

    초기 커밋

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include "BunnyModel.h"
+
+void MyDisplay() {
+    const int FACE_INDICES_LENGTH = 16301;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_LINES);
+    for (int i = 0; i < FACE_INDICES_LENGTH; i++) {
+        int x = face_indicies[i][0];
+        int y = face_indicies[i][1];
+        int z = face_indicies[i][2];
+
+        glVertex3fv(vertices[x]);
+        glVertex3fv(vertices[y]);
+        glVertex3fv(vertices[z]);
+        glVertex3fv(vertices[y]);
+        glVertex3fv(vertices[z]);
+        glVertex3fv(vertices[x]);
+    }
+    glEnd();
+    glFlush(); }
+int main(int argc, char *argv[]) {
+    glutInit(&argc, argv);
+    glutCreateWindow("OpenGL Example");
+    glutDisplayFunc(MyDisplay);
+    glutMainLoop();
+    return 0; }
