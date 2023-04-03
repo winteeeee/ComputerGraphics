8d98a63096bd738d5cdde1de9c7de1ae1112c9f9
winteeeee <b12e34e56@naver.com>
2023-04-04 01:09:42 +0900

    1차 구현 완료

--- 과제1/AnalogWatch.cpp
+++ 과제1/AnalogWatch.cpp
@@ -1,26 +1,69 @@
 #include <glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
-#include <time.h> //현재 시각 구하기 위함
-#include <math.h> //삼각함수
-// 현재 시간을 각도로 변환 -> 그 각도일 때 좌표값 계산
-//(rcos세타, rsin세타)
-//시계의 1, 2, 3, 4 등의 숫자를 360도에 매핑
+#include <ctime>
+#include <cmath>
 
-void MyDisplay()
-{
+const double RADIUS = 0.9;
+const double HOUR_HAND_LENGTH = 0.5;
+const double MINUTE_HAND_LENGTH = 0.7;
+const double SECOND_HAND_LENGTH = 0.8;
+double hour = 0;
+double minute = 0;
+double second = 0;
+
+void MyDisplay() {
     glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
-    glColor3f(1.0f, 0.0f, 0.0f);
+    glLineWidth(5.0);
+    glColor3f(1.0, 1.0, 1.0);
+
+    glBegin(GL_LINE_LOOP);  //원 그리기
+        for (int i = 0; i < 360; i++) {
+            double angle = i * M_PI / 180;
+            glVertex2d(RADIUS * cos(angle), RADIUS * sin(angle));
+        }
+    glEnd();
+
+    time_t cur = time(nullptr);
+    struct tm* time = localtime(&cur);
+    hour = -time->tm_hour;  //시간이 지날 수록 각도가 줄어들어야 하므로 음수화
+    minute = -time->tm_min;
+    second = -time->tm_sec;
+    double secondAngle = (second + 15) * 6 * M_PI / 180;    //60초당 360도를 돌아야하므로 1초당 6도 배정.
+    double minuteAngle = (minute + 15) * 6 * M_PI / 180 + secondAngle / 60; //마찬가지로 1분당 6도 배정. 이후 6도의 범위 내에서 초의 영향을 360/6만큼 받는다.
+    double hourAngle = (hour + 3) * 30 * M_PI / 180 + minuteAngle / 12; //12시당 360도를 돌아야하므로 1시당 30도 배정. 이후 30도의 범위 내에서 분의 영향을 360/30만큼 받는다.
+
+    glBegin(GL_LINES);
+        glVertex2d(0.0, 0.0);
+        glVertex2d(HOUR_HAND_LENGTH * cos(hourAngle), HOUR_HAND_LENGTH * sin(hourAngle));
+    glEnd();
+
+    glBegin(GL_LINES);
+        glVertex2d(0.0, 0.0);
+        glVertex2d(MINUTE_HAND_LENGTH * cos(minuteAngle), MINUTE_HAND_LENGTH * sin(minuteAngle));
+    glEnd();
+
+    glBegin(GL_LINES);
+        glVertex2d(0.0, 0.0);
+        glVertex2d(SECOND_HAND_LENGTH * cos(secondAngle), SECOND_HAND_LENGTH * sin(secondAngle));
+    glEnd();
 
     glFlush();
 }
 
-int main(int argc, char *argv[])
-{
+void MyTimer(int val) {
+    glutPostRedisplay();
+    glutTimerFunc(1000, MyTimer, 1);
+}
+
+int main(int argc, char *argv[]) {
     glutInit(&argc, argv);
+    glutInitWindowSize(700, 700);
     glutCreateWindow("OpenGL Example");
+
     glutDisplayFunc(MyDisplay);
+    glutTimerFunc(1000, MyTimer, 1);
     glutMainLoop();
+
     return 0;
 }
