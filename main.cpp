eaefc3b4b245cafe938ed039297a561afddcc525
winteeeee <b12e34e56@naver.com>
2023-04-12 15:38:42 +0900

    성 만들기

--- main.cpp
+++ main.cpp
@@ -1,84 +1,120 @@
 #include <glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
-#include <cmath>
 
-double b, c, t1, t2, x, y;
-double a = 1;
-double d = 1;
-double scale = 1;
-double theta = 0;
-
-void transform(double *X, double *Y) {
-    double ix = *X;
-    double iy = *Y;
-
-    *X = a * ix + b * iy + t1;
-    *Y = c * ix + d * iy + t2;
-}
+GLint xValue = 0;
+GLint yValue = 0;
+GLint zValue = 0;
+GLint clickDown = 0;
+GLint fixX = 0;
+GLint fixY = 0;
 
 void MyDisplay() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3f(1.0, 0.0, 0.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 
-    x = -0.1; y = -0.1;
-    transform(&x, &y);
-    glVertex2d(x, y);
+    glRotatef(xValue, 1.0, 0.0, 0.0);
+    glRotatef(yValue, 0.0, 1.0, 0.0);
+    glRotatef(zValue, 0.0, 0.0, 1.0);
 
-    x = -0.1; y = 0.1;
-    transform(&x, &y);
-    glVertex2d(x, y);
-
-    x = 0.1; y = 0.1;
-    transform(&x, &y);
-    glVertex2d(x, y);
-
-    x = 0.1; y = -0.1;
-    transform(&x, &y);
-    glVertex2d(x, y);
+    glBegin(GL_LINES);
+    glColor3f(1, 0, 0); // X
+    glVertex3f(-1, 0, 0); glVertex3f(1, 0, 0);
+    glColor3f(0, 1, 0); // Y
+    glVertex3f(0, -1, 0); glVertex3f(0, 1, 0);
+    glColor3f(0, 0, 1); // Z
+    glVertex3f(0, 0, -1); glVertex3f(0, 0, 1);
     glEnd();
+
+    glPushMatrix();
+    glColor3f(1, 0, 0);
+    glTranslatef(-0.2, 0, 0);
+    glutSolidCube(0.4);
+    glTranslatef(0.4, 0, 0);
+    glutSolidCube(0.4);
+    glColor3f(0, 0, 1);
+    glTranslatef(0.4, 0, 0);
+    glutSolidCube(0.4);
+    glTranslatef(-1.2, 0, 0);
+    glutSolidCube(0.4);
+    glPopMatrix();
+    glColor3f(1, 1, 0);
+    glPushMatrix();
+    glTranslatef(0.6, 0.2, 0);
+    glRotatef(-90, 1, 0, 0);
+    glutSolidCone(0.2, 0.3, 10, 10);
+    glPopMatrix();
+    glTranslatef(-0.6, 0.2, 0);
+    glRotatef(-90, 1, 0, 0);
+    glutSolidCone(0.2, 0.3, 10, 10);
     glFlush();
 }
 
-void update() {
-    double angle = theta * M_PI / 180;
-    a = scale * cos(angle);
-    b = -scale * sin(angle);
-    c = -b;
-    d = a;
-}
-
-void MyKeyboard(unsigned char key, int X, int Y) {
-    if (key == 's') {
-        a += 0.1;
-        d += 0.1;
-    } else if (key == 'S') {
-        a -= 0.1;
-        d -= 0.1;
-    } else if (key == 't') {
-        theta += 1;
-        update();
-    } else if (key == 'T') {
-        theta -= 1;
-        update();
-    } else if (key == 'r') {
-
-    } else if (key == 'R') {
-
+void MyKeyboard(unsigned char KeyPressed, int X, int Y)
+{
+    switch (KeyPressed){
+        case 'X':
+        case 'x':    xValue += 1;
+            break;
+        case 'Y':
+        case 'y':    yValue += 1;
+            break;
+        case 'Z':
+        case 'z':    zValue += 1;
+            break;
+        case ' ':
+            xValue = 0;
+            yValue = 0;
+            zValue = 0;
+            break;
     }
     glutPostRedisplay();
 }
 
-int main(int argc, char** argv)
+void MyMouseClick(GLint Button, GLint State, GLint X, GLint Y)
 {
-    a = 1;
-    d = 1;
+    if (Button == GLUT_LEFT_BUTTON && State == GLUT_DOWN) {
+        fixX = X;
+        fixY = Y;
+        clickDown = 1;
+    }
+
+    if (Button == GLUT_LEFT_BUTTON && State == GLUT_UP) {
+        clickDown = 0;
+    }
+}
+
+void MyMouseMove(GLint X, GLint Y)
+{
+    if (clickDown == 1) {
+        xValue = xValue + (fixX - X);
+        yValue = yValue + (fixY - Y);
+        fixX = X;
+        fixY = Y;
+    }
+
+    glutPostRedisplay();
+}
+
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitWindowSize(700, 700);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
     glutCreateWindow("OpenGL Example");
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glMatrixMode(GL_PROJECTION); // 차후 설명
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
+    glEnable(GL_DEPTH_TEST);
 
     glutDisplayFunc(MyDisplay);
     glutKeyboardFunc(MyKeyboard);
+    glutMouseFunc(MyMouseClick);
+    glutMotionFunc(MyMouseMove);
     glutMainLoop();
     return 0;
 }
